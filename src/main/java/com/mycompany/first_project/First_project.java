aadc0d311f3ed687df09b28d6df7bfcb311dbc62
Behruz Veliyev <Behruz.Veliyev@bazarstore.az>
2023-02-10 10:09:43 +0400

    Initial commit

---
+++ src/main/java/com/mycompany/first_project/First_project.java
@@ -0,0 +1,31 @@
+package com.mycompany.first_project;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class First_project {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("a-ni daxil edin: ");
+        double a = sc.nextDouble();
+
+        System.out.println("b-ni daxil edin: ");
+        double b = sc.nextDouble();
+
+
+        System.out.println("Istifadeci daxil etdi: "+(a+b));
+
+
+
+
+
+
+
+
+
+
+    }
+}
