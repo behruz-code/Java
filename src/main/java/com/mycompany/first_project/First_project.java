647f0fbe023421f4e09c2e0981bb9042bc83e3b7
Behruz.Veliyev <behruzveliyevv@gmail.com>
2023-02-12 18:13:05 +0400

    daha yeni

--- src/main/java/com/mycompany/first_project/First_project.java
+++ src/main/java/com/mycompany/first_project/First_project.java
@@ -1,31 +1,17 @@
 package com.mycompany.first_project;
-
-import java.io.Console;
 import java.util.Scanner;
-
 public class First_project {
-
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
+        while(true){
+            System.out.println("hi");
 
-        System.out.println("a-ni daxil edin: ");
-        double a = sc.nextDouble();
-
-        System.out.println("b-ni daxil edin: ");
-        double b = sc.nextDouble();
-
-
-        System.out.println("Istifadeci daxil etdi: "+(a+b));
-
-
-
-
-
-
-
+        }
 
 
 
     }
 }
+
+
+
---
+++ src/main/java/com/mycompany/first_project/Method.java
@@ -0,0 +1,15 @@
+package com.mycompany.first_project;
+
+public class Method {
+
+    public static void main(String[] args) {
+         double hesabla = topla(27,28);
+        System.out.println(hesabla);
+    }
+
+    public static double topla(double a,double b){
+        return a + b;
+
+    }
+
+}
